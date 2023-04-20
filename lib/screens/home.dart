8ae01a3a14d6c890cebcee42a1c0f0708361d4ac
KlonAnon <klonanon.com@gmail.com>
2023-04-20 15:13:34 +0200

    Fixed some weird formatting & added linter rules
    Fixed some weird formatting caused by not adding trailing commas
    (visit: https://stackoverflow.com/questions/63185093/dart-format-is-weird-in-vscode
    and
    https://docs.flutter.dev/development/tools/formatting#using-trailing-commas)
    also added a linter rule, so that wont happen again + linter rule to
    prefer single quote Strings.
    
    Signed-off-by: KlonAnon <klonanon.com@gmail.com>

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,32 +6,39 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            keyboardType: TextInputType.multiline,
-            minLines: 3,
-            maxLines: 5,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Type in what you want to create',
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              keyboardType: TextInputType.multiline,
+              minLines: 3,
+              maxLines: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Type in what you want to create',
+              ),
             ),
-          )),
-      ElevatedButton(
-          onPressed: () async {
-            bool hasInternet = await checkInternet();
-            if (hasInternet) {
-              print('Your connected');
-            } else {
-              if (context.mounted) {
-                showDialog<String>(
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              bool hasInternet = await checkInternet();
+              if (hasInternet) {
+                print('Your connected');
+              } else {
+                if (context.mounted) {
+                  showDialog<String>(
                     context: context,
-                    builder: (BuildContext context) => NoInternetDialog());
+                    builder: (BuildContext context) => NoInternetDialog(),
+                  );
+                }
               }
-            }
-          },
-          child: Text('Generate'))
-    ]));
+            },
+            child: Text('Generate'),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/no_internet.dart
+++ lib/widgets/no_internet.dart
@@ -4,18 +4,21 @@ class NoInternetDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Row(children: [
-        Icon(Icons.warning, color: Colors.yellow),
-        SizedBox(width: 8.0),
-        Text('No Internet Connection')
-      ]),
+      title: Row(
+        children: [
+          Icon(Icons.warning, color: Colors.yellow),
+          SizedBox(width: 8.0),
+          Text('No Internet Connection'),
+        ],
+      ),
       content: Text('Please check your internet connection and try again.'),
       actions: [
         TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('OK'))
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('OK'),
+        ),
       ],
     );
   }
